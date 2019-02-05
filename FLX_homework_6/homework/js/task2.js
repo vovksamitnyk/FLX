b596c3b038625d64778877a3835e30e78d1c8a18
Dmytro Yatsyshyn <vovksamitnyk@gmail.com>
2019-02-05 21:29:55 +0200

    Fix bugs

--- FLX_homework_6/homework/js/task2.js
+++ FLX_homework_6/homework/js/task2.js
@@ -1,20 +1,20 @@
-let inputPriсe, inputDiscount, correctInputData;
+let inputPrice, inputDiscount, correctInputData;
 
 correctInputData = true;
 
-inputPriсe = parseFloat(prompt("Input priсe please:"));
-if (!isNaN(inputPriсe) && inputPriсe >= 0 && inputPriсe <= 9999999) {
+inputPrice = parseFloat(prompt("Input price please:"));
+if (!isNaN(inputPrice) && inputPrice >= 0 && inputPrice <= 9999999) {
     inputDiscount = parseFloat(prompt("Input discount (0-99%):"));
 } else {
     correctInputData = false;
 }
 if (!isNaN(inputDiscount) && inputDiscount >= 0 && inputDiscount <= 99 && correctInputData) {
-    let saved = inputPriсe * inputDiscount / 100;
+    let saved = inputPrice * inputDiscount / 100;
     saved = +saved.toFixed(2);
-    let priseWithDiscnt = inputPriсe - saved;
-    priсeWithDiscnt = +priсeWithDiscnt.toFixed(2);
-    alert("Price without discount:  " + inputPriсe + "\nDiscount:  " + inputDiscount
-          + "%\nPrice with discount:  " + priсeWithDiscnt + "\nSaved:  " + saved);
+    let priceWithDiscnt = inputPrice - saved;
+    priceWithDiscnt = +priceWithDiscnt.toFixed(2);
+    alert("Price without discount:  " + inputPrice + "\nDiscount:  " + inputDiscount
+          + "%\nPrice with discount:  " + priceWithDiscnt + "\nSaved:  " + saved);
 } else {
     alert("invalid input data");
 }
